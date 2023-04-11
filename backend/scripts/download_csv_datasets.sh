0cae24a7cd54aaa96c62f1129b6476f5040c1841
Ruslan Talpa <ruslan.talpa@subzero.cloud>
2023-04-11 11:34:44 +0300

    implement csv limit

--- backend/scripts/download_csv_datasets.sh
+++ backend/scripts/download_csv_datasets.sh
@@ -7,6 +7,74 @@ api_endpoint="https://catalog.data.gov/api/3/action/package_search"
 query="res_format:CSV"
 rows=100
 start=0
+max_file_size=104857600 # 100MB
+
+# set some options based on OS
+if [[ "$(uname)" == "Darwin" ]]; then
+    stat_options="-f%z"
+    date_options="-jf \"%Y-%m-%dT%H:%M:%S\""
+else
+    stat_options="-c%s"
+    date_options="-d"
+fi
+
+# Function to monitor file size and kill curl if the size exceeds the limit
+monitor_file_size() {
+    local curl_pid=$1
+    local file_name=$2
+    while true; do
+        sleep 1
+        if [ -e "${file_name}" ]; then
+            current_size=$(stat ${stat_options} "${file_name}")
+            if [ "${current_size}" -ge "${max_file_size}" ]; then
+                kill ${curl_pid} &> /dev/null
+                break
+            fi
+        fi
+    done
+}
+
+# Function to download a file with curl, monitoring its size during the download
+download_file() {
+    local file_name=$1
+    local resource_url=$2
+    local temp_status_code_file=$(mktemp)
+    local http_status_code=0
+
+    # Start the download in the background and store the curl process ID
+    (curl -s -L --write-out "%{http_code}" -o "${file_name}" "${resource_url}") 1>"${temp_status_code_file}" & curl_pid=$!
+
+    # Start the file size monitor function in the background
+    monitor_file_size ${curl_pid} "${file_name}" & monitor_pid=$!
+
+    # Wait for the curl process to finish
+    wait ${curl_pid} &> /dev/null
+    curl_exit_code=$?
+
+    # Get the actual HTTP status code from the temporary file
+    http_status_code=$(cat "${temp_status_code_file}")
+    rm "${temp_status_code_file}"
+
+    # Kill the monitor process and remove the output file if the size limit was exceeded
+    kill ${monitor_pid} &> /dev/null
+    if [ "${curl_exit_code}" -eq "143" ]; then
+        rm "${file_name}"
+        # echo "File is too big, download canceled. ${curl_exit_code}"
+        # return 412
+        echo "412"
+    else
+        file_size=$(stat ${stat_options} "${file_name}")
+        # echo "Download complete (${file_size} bytes)"
+        # return ${http_status_code}
+        echo "${http_status_code}"
+    fi
+}
+
+# Call the download_file function with the desired file_name and resource_url
+# file_name="output_file"
+# resource_url="https://example.com/large-file"
+# download_file "${file_name}" "${resource_url}"
+
 
 # Change directory to SQLITE_DATASETS directory if the env var is set
 if [ ! -z "$SQLITE_DATASETS" ]; then
@@ -59,38 +127,32 @@ while [ $start -lt $total_datasets ]; do
         
         if [[ -f "${file_name}" ]]; then
           file_last_modified=$(date -r "${file_name}" +%s)
-          if [[ "$(uname)" == "Darwin" ]]; then
-              # macOS
-              resource_last_modified_epoch=$(date -jf "%Y-%m-%dT%H:%M:%S" "${resource_last_modified}" +%s)
-          else
-              # Linux
-              resource_last_modified_epoch=$(date -d "${resource_last_modified}" +%s)
-          fi
-
+          resource_last_modified_epoch=$(date ${date_options} "${resource_last_modified}" +%s)
           if [[ $resource_last_modified_epoch -gt $file_last_modified ]]; then
             echo "Updating ${file_name} from ${resource_url}"
-            http_status_code=$(curl -s -L -w "%{http_code}" -o "${file_name}" "${resource_url}")
+            # http_status_code=$(curl -s -L --max-filesize $max_file_size -w "%{http_code}" -o "${file_name}" "${resource_url}")
+            http_status_code=$(download_file "${file_name}" "${resource_url}")
 
             if [[ $http_status_code -ge 400 ]]; then
-              echo "Error downloading ${file_name}: HTTP status code ${http_status_code}"
-              exit 1
+              echo "Error: HTTP status code ${http_status_code}"
+            else
+              # Save JSON metadata
+              echo "${dataset_metadata}" > "${metadata_file_name}"
             fi
-            
-            # Save JSON metadata
-            echo "${dataset_metadata}" > "${metadata_file_name}"
           else
-            echo "${file_name} is already up-to-date."
+            echo "already up-to-date."
           fi
         else
           echo "Downloading ${file_name} from ${resource_url}"
-          http_status_code=$(curl -s -L -w "%{http_code}" -o "${file_name}" "${resource_url}")
+          # http_status_code=$(curl -s -L --max-filesize $max_file_size -w "%{http_code}" -o "${file_name}" "${resource_url}")
+          http_status_code=$(download_file "${file_name}" "${resource_url}")
 
           if [[ $http_status_code -ge 400 ]]; then
-            echo "Error downloading ${file_name}: HTTP status code ${http_status_code}"
-            exit 1
+            echo "Error: HTTP status code ${http_status_code}"
+          else
+            # Save JSON metadata
+            echo "${dataset_metadata}" > "${metadata_file_name}"
           fi
-          # Save JSON metadata
-          echo "${dataset_metadata}" > "${metadata_file_name}"
         fi
 
         # Update progress
